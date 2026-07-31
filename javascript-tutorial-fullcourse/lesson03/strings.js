6e19cab9b0cbb168d7c1cccc6a50947dcd798931
Jennifer Sheng <122578357+sheng52@users.noreply.github.com>
2026-07-31 13:42:49 -0700

    Lesson 3

---
+++ javascript-tutorial-fullcourse/lesson03/strings.js
@@ -0,0 +1,25 @@
+'hello'
+'some' + 'text'
+'some' + 'more' + 'text'
+
+typeof 2 // number
+typeof 'hello' // string
+
+'hello + 3' // 'hello3'
+
+'$' + 20.95 + 7.99 // no
+'$' + (20.95 + 7.99)
+'$' + (2095 + 799) / 100
+
+'Items (' + (1 + 1) + '): $' + (2095 + 799) / 100
+
+alert('Items (' + (1 + 1) + '): $' + (2095 + 799) / 100);
+
+"I'm learning JavaScript"
+'I\'m learning Javascript'
+alert('some\ntext');
+
+`Items (${1 + 1}): $${(2095 + 799) / 100}`
+
+`some
+text` // 'some\ntext'
