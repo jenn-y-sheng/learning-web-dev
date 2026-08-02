f8815df540de41f630e4b48b241b0d635a02687b
Jennifer Sheng <122578357+sheng52@users.noreply.github.com>
2026-08-02 15:13:49 -0700

    14e

--- javascript-tutorial-fullcourse/javascript-amazon-project-main/scripts/checkout.js
+++ javascript-tutorial-fullcourse/javascript-amazon-project-main/scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart, removeFromCart} from '../data/cart.js';
+import {cart, removeFromCart, calculateCartQuantity} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
@@ -116,12 +116,6 @@ document.querySelectorAll('.js-delete-link')
   });
 
 function updateCartQuantity() {
-  let cartQuantity = 0;
-
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
-  });
-
   document.querySelector('.js-return-to-home-link')
-    .innerHTML = `${cartQuantity} items`;
+    .innerHTML = `${calculateCartQuantity()} items`;
 }
