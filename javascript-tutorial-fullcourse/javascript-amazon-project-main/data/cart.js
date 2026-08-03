604b228de4357cad0eb531c74e86b1e8eac7e416
Jennifer Sheng <122578357+sheng52@users.noreply.github.com>
2026-08-03 15:18:05 -0700

    16l

--- javascript-tutorial-fullcourse/javascript-amazon-project-main/data/cart.js
+++ javascript-tutorial-fullcourse/javascript-amazon-project-main/data/cart.js
@@ -87,6 +87,10 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
     }
   });
 
+  if (!matchingItem) {
+    return;
+  }
+
   matchingItem.deliveryOptionId = deliveryOptionId;
 
   saveToStorage();
--- javascript-tutorial-fullcourse/javascript-amazon-project-main/tests/data/cartTest.js
+++ javascript-tutorial-fullcourse/javascript-amazon-project-main/tests/data/cartTest.js
@@ -107,5 +107,14 @@ describe('test suite: updateDeliveryOption()', () => {
         quantity: 1,
         deliveryOptionId: '2'
     }]));
-  })
+  });
+
+  it('updates delivery option of a product not in the cart', () => {
+    updateDeliveryOption('does-not-exist', '2');
+    expect(cart.length).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(0);
+    expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart[0].quantity).toEqual(1);
+    expect(cart[0].deliveryOptionId).toEqual('1');
+  });
 });
