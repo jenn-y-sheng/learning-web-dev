b393e382ac85ee10685698d95f0d40a835de4a97
Jennifer Sheng <122578357+sheng52@users.noreply.github.com>
2026-08-03 15:15:37 -0700

    16k

--- javascript-tutorial-fullcourse/javascript-amazon-project-main/tests/data/cartTest.js
+++ javascript-tutorial-fullcourse/javascript-amazon-project-main/tests/data/cartTest.js
@@ -1,4 +1,4 @@
-import { addToCart, cart, loadFromStorage, removeFromCart } from "../../data/cart.js";
+import { addToCart, cart, loadFromStorage, removeFromCart, updateDeliveryOption } from "../../data/cart.js";
 
 describe('test suite: addToCart', () => {
   beforeEach(() => {
@@ -80,4 +80,32 @@ describe('test suite: removeFromCart()', () => {
         deliveryOptionId: '1'
     }]));
   });
+});
+
+describe('test suite: updateDeliveryOption()', () => {
+  beforeEach(() => {
+    spyOn(localStorage, 'setItem');
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([{
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 1,
+        deliveryOptionId: '1'
+      }]);
+    });
+    loadFromStorage();
+  });
+
+  it('updates delivery option of a product in the cart', () => {
+    updateDeliveryOption('e43638ce-6aa0-4b85-b27f-e1d07eb678c6', '2');
+    expect(cart.length).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart[0].quantity).toEqual(1);
+    expect(cart[0].deliveryOptionId).toEqual('2');
+    expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([{
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 1,
+        deliveryOptionId: '2'
+    }]));
+  })
 });
