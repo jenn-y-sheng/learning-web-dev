2a7b9353a2f4168be95947536ac292dbfd45c9b4
Jennifer Sheng <122578357+sheng52@users.noreply.github.com>
2026-08-03 15:24:55 -0700

    16m

--- javascript-tutorial-fullcourse/javascript-amazon-project-main/tests/data/cartTest.js
+++ javascript-tutorial-fullcourse/javascript-amazon-project-main/tests/data/cartTest.js
@@ -109,8 +109,17 @@ describe('test suite: updateDeliveryOption()', () => {
     }]));
   });
 
-  it('updates delivery option of a product not in the cart', () => {
+  it('does nothing if product id does not exist', () => {
     updateDeliveryOption('does-not-exist', '2');
+    expect(cart.length).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(0);
+    expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart[0].quantity).toEqual(1);
+    expect(cart[0].deliveryOptionId).toEqual('1');
+  });
+
+  it('does nothing if delivery option does not exist', () => {
+    updateDeliveryOption('e43638ce-6aa0-4b85-b27f-e1d07eb678c6', 'does-not-exist');
     expect(cart.length).toEqual(1);
     expect(localStorage.setItem).toHaveBeenCalledTimes(0);
     expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
