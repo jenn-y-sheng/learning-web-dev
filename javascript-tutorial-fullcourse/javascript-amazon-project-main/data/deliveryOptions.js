2c6272adad6292f25dc1276604e44f90c9b7add2
Jennifer Sheng <122578357+sheng52@users.noreply.github.com>
2026-08-02 22:09:30 -0700

    15m

--- javascript-tutorial-fullcourse/javascript-amazon-project-main/data/deliveryOptions.js
+++ javascript-tutorial-fullcourse/javascript-amazon-project-main/data/deliveryOptions.js
@@ -26,12 +26,22 @@ export function getDeliveryOption(deliveryOptionId) {
   return deliveryOption || deliveryOptions[0];
 }
 
+function isWeekend(date) {
+  const formatted = date.format('dddd');
+  return formatted === 'Saturday' || formatted === 'Sunday';
+}
+
 export function calculateDeliveryDate(deliveryOption) {
-  const today = dayjs();
-  const deliveryDate = today.add(
-    deliveryOption.deliveryDays,
-    'days'
-  );
+  let daysToAdd = deliveryOption.deliveryDays;
+  let deliveryDate = dayjs();
+
+  while (daysToAdd > 0) {
+    deliveryDate = deliveryDate.add(1, 'day');
+    if (!isWeekend(deliveryDate)) {
+      daysToAdd--;
+    }
+  }
+
   const dateString = deliveryDate.format(
     'dddd, MMMM D'
   );
