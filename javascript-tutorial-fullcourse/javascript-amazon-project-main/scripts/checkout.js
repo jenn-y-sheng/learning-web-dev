bf35c2f091895f81969d78532f7702293236bde9
Jennifer Sheng <122578357+sheng52@users.noreply.github.com>
2026-08-02 17:41:42 -0700

    14k

--- javascript-tutorial-fullcourse/javascript-amazon-project-main/scripts/checkout.js
+++ javascript-tutorial-fullcourse/javascript-amazon-project-main/scripts/checkout.js
@@ -42,7 +42,7 @@ cart.forEach((cartItem) => {
             <span class="update-quantity-link link-primary js-update-quantity-link" data-product-id="${matchingProduct.id}">
               Update
             </span>
-            <input class="quantity-input">
+            <input class="quantity-input js-quantity-input-${matchingProduct.id}">
             <span class="save-quantity-link link-primary js-save-quantity-link" data-product-id="${matchingProduct.id}">Save</span>
             <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
               Delete
@@ -139,5 +139,8 @@ document.querySelectorAll('.js-save-quantity-link')
 
       const cartItemContainer = document.querySelector(`.js-cart-item-container-${productId}`);
       cartItemContainer.classList.remove('is-editing-quantity');
+
+      const inputElement = document.querySelector(`.js-quantity-input-${productId}`);
+      const newQuantity = Number(inputElement.value);
     })
   })
