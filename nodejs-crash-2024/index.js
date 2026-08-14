352119d4e73c8e3bfbd1761fb534f59946b6155b
Jennifer Sheng <122578357+sheng52@users.noreply.github.com>
2026-08-13 20:07:01 -0700

    Starting Node.js crash course, testing .gitignore file

---
+++ nodejs-crash-2024/index.js
@@ -0,0 +1,9 @@
+import getPosts, { getPostsLength } from "./postController.js";
+
+console.log(getPosts());
+console.log(`Posts Length: ${getPostsLength()}`);
+
+// const {generateRandomNumber, celsiusToFahrenheit} = require('./utils');
+
+// console.log(`Random Number: ${generateRandomNumber()}`);
+// console.log(`Celsius to fahrenheit: ${celsiusToFahrenheit(0)}`);
---
+++ nodejs-crash-2024/server.js
@@ -0,0 +1,14 @@
+import http from 'http';
+const PORT = 5001;
+
+const server = http.createServer((req, res) => {
+  // res.setHeader('Content-Type', 'text/html');
+  // res.statusCode = 404;
+
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+  res.end('<h1>Hello World</h1>');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
